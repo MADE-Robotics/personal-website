3fc9a6b9071fe6e87cc43a1b68146ae184bafbf5
Mira Heckmann <miraheckmann@gmail.com>
2021-03-16 16:12:37 -0700

    adds projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,29 +1,73 @@
 import React from "react"
 import styles from './Projects.module.scss';
+import test from "../../imgs/territories.png";
+import pokemon from "../../imgs/pokemon.png";
+import mars from "../../imgs/marsrealty.png";
+import blog from "../../imgs/blog.png";
+import chatApp from "../../imgs/chatApp.png";
+import shooter from "../../imgs/shooter.jpg";
+// import {Button, OverlayTrigger, render, Tooltip } from 'react'
+
 
 export default function Projects() {
-    return (
-      <div id="projects" className={styles.Projects}>
+
+
+  return (
+      
+    <div id="projects" className={styles.Projects}>
         <h3>Projects</h3>
 
-        <ul>
-          <li>
-            <a target="_blank" rel="noreferrer" href="https://github.com/Mheckmann7/Chat-App">Chat App</a>
+        <ul class="card-deck">
+          <li class="card bg-dark">
+            <a target="_blank" rel="noreferrer" href="https://github.com/Mheckmann7/Chat-App">
+              <img class="card-img-top" src={chatApp} alt="chat app" /></a>
+            <div class="card-body bg-secondary">
+              <h5 class="card-title text-white d-inline">Chat App</h5>
+              <p class="card-text text-white">Full CRUD blog site created with Python and Django</p>
+            </div>
+          </li >
+          <li class="card bg-dark"> 
+            <a target="_blank" rel="noreferrer" href="https://github.com/Mheckmann7/blog-python">
+              <img class="card-img-top" src={blog} alt="territories" />
+            
+            </a>
+              <div class="card-body bg-secondary">
+              <h5 class="card-title text-white d-inline">Blog Site</h5>
+              <p class="card-text text-white">Full CRUD blog site created with Python and Django</p>
+            </div>
           </li>
-          <li>
-            <a target="_blank" rel="noreferrer" href="https://github.com/Mheckmann7/blog-python">Python Dajango Blog</a>
+          <li class="card bg-dark">
+          <a target="_blank" rel="noreferrer" href="https://github.com/Mheckmann7/Territories-client">
+                <img class="card-img-top" src={test} alt="territories" /></a>
+            <div class="card-body bg-secondary">
+              <h5 class="card-title text-white d-inline">Territories</h5>
+              <p class="card-text text-white">Created with React and Google MapsAPI</p>
+            </div>
+          </li >
+          <li class="card bg-dark">
+            <a target="_blank" rel="noreferrer" href="https://github.com/Mheckmann7/Pokemon-Team-Builder" >
+              <img class="card-img-top" src={pokemon} alt="territories" /></a>
+              <div class="card-body bg-secondary">
+              <h5 class="card-title text-white d-inline">Pokemon Team Builder</h5>
+              <p class="card-text text-white">Created with HTML and JS</p>
+            </div>
           </li>
-          <li>
-            <a target="_blank" rel="noreferrer" href="https://github.com/Mheckmann7/Territories-client">Territories</a>
+          <li class="card bg-dark">
+            <a target="_blank" rel="noreferrer" href="https://github.com/Mheckmann7/Mars-Real-Estate">
+              <img class="card-img-top" src={mars} alt="territories" /></a>
+              <div class="card-body bg-secondary">
+              <h5 class="card-title text-white d-inline">Pokemon Team Builder</h5>
+                <p class="card-text text-white">Created with HTML and JS</p> 
+              </div>
+              
           </li>
-          <li>
-            <a target="_blank" rel="noreferrer" href="https://github.com/Mheckmann7/Pokemon-Team-Builder" >Pokemon Team Builder</a>  
-          </li>
-          <li>
-            <a target="_blank" rel="noreferrer" href="https://github.com/Mheckmann7/Mars-Real-Estate">Mars Realestate Site</a>
-          </li>
-          <li>
-            <a target="_blank" rel="noreferrer" href="https://github.com/Mheckmann7/Unity-3rd-Person-Shooter-Game">Unity 3rd Person Shooter Controls</a>
+          <li class="card bg-dark">
+            <a target="_blank" rel="noreferrer" href="https://github.com/Mheckmann7/Unity-3rd-Person-Shooter-Game">
+            <img class="card-img-top" src={shooter} alt="territories" /></a>
+              <div class="card-body bg-secondary">
+              <h5 class="card-title text-white d-inline">Unity 3rd Person Controller</h5>
+              <p class="card-text text-white">A project to manually create a 3rd person controller in Unity using C#</p>
+            </div>
           </li>
           </ul>
 
---
+++ src/components/Tooltip/Tooltip.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Button, OverlayTrigger, render } from 'react'
+
+    // const renderTooltip = (props) => (
+    //     <Tooltip id="button-tooltip" {...props}>
+    //       Simple tooltip
+    //     </Tooltip>
+    //   );
+      
+    //   render(
+    //     <OverlayTrigger
+    //       placement="right"
+    //       delay={{ show: 250, hide: 400 }}
+    //       overlay={renderTooltip}
+    //     >
+    //       <Button variant="success">Hover me to see</Button>
+    //     </OverlayTrigger>,
+    //   );
+
